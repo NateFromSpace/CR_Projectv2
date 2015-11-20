ffa1efcaced86db150e858b5ddf9e2807dd3123a
NateFromSpace <nathanwangler21@gmail.com>
2015-11-19 21:43:56 -0600

    Enemy Added

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,6 +11,8 @@ public class LevelManager : MonoBehaviour {
 	public GameObject respawnParticle;
 	public float respawnDelay;
 
+	private float gravityStore;
+
 	public int pointPenaltyOnDeath;
 
 	// Use this for initialization
@@ -35,10 +37,13 @@ public class LevelManager : MonoBehaviour {
 		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
 		player.enabled = false;
 		player.renderer.enabled = false;
+		gravityStore = player.rigidbody2D.gravityScale;
+		player.rigidbody2D.gravityScale = 0f;
 		player.rigidbody2D.velocity = Vector2.zero;
 		ScoreManager.AddPoints (-pointPenaltyOnDeath);
 		Debug.Log("Player Respawn");
 		yield return new WaitForSeconds (respawnDelay);
+		player.rigidbody2D.gravityScale = gravityStore;
 		player.transform.position = currentCheckpoint.transform.position;
 		player.enabled = true;
 		player.renderer.enabled = true;
